9c98017b74c19b75313cd3f2020135df8f2be994
Timothee Flutre <timflutre@gmail.com>
2013-10-18 23:53:27 -0500

    improve portability to Mac OS X

--- src/utils.h
+++ src/utils.h
@@ -31,6 +31,18 @@ using namespace std;
 
 #include "zlib.h"
 
+#ifdef __APPLE__
+#define __MAC_10_8 1080
+#include <Availability.h>
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_8
+#define CONST_DIRENT_T struct dirent
+#else
+#define CONST_DIRENT_T const struct dirent
+#endif
+#else
+#define CONST_DIRENT_T const struct dirent
+#endif
+
 vector<string> & split (const string & s, char delim, vector<string> & tokens);
 
 vector<string> split (const string & s, char delim);
